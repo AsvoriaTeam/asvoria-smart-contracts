e6575d76afeba1928dc20294a603f6e302c7c881
Azim Baig <abdec2@hotmail.com>
2024-09-11 14:47:30 +0300

    Update utils.rs

--- solana-contracts/token_launchpad/programs/launchpad_factory/src/utils.rs
+++ solana-contracts/token_launchpad/programs/launchpad_factory/src/utils.rs
@@ -2,9 +2,9 @@ use anchor_lang::prelude::*;
 use anchor_lang::solana_program::{program::invoke, system_instruction};
 
 pub fn transfer_fee(
-    from: AccountInfo,
+    from: &AccountInfo,
     to: Pubkey,
-    system_program: AccountInfo,
+    system_program: &AccountInfo,
     amount: u64,
 ) -> Result<()> {
     let ix = system_instruction::transfer(from.key, &to, amount);
