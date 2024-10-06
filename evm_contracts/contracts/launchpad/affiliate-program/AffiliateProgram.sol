0f232a74909fcd98b56d99d8720ddecb70b2238a
Azim Baig <abdec2@hotmail.com>
2024-10-06 04:07:16 +0300

    Update AffiliateProgram.sol

--- evm_contracts/contracts/launchpad/affiliate-program/AffiliateProgram.sol
+++ evm_contracts/contracts/launchpad/affiliate-program/AffiliateProgram.sol
@@ -1,11 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
 
-contract AffiliateProgram is Initializable, ReentrancyGuardUpgradeable, OwnableUpgradeable {
+contract AffiliateProgram is OwnableUpgradeable {
     uint256 public commissionRate = 500;
 
     mapping(address => address) public referrerOf; // referred -> referrer
